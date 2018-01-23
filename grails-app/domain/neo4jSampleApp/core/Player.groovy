ee1e7270fb7559fe3f0490590563df4796750926
Amalia <amaliavillanueva@gmail.com>
2018-01-23 15:32:45 -0300

    add attributes property

--- grails-app/domain/neo4jSampleApp/core/Player.groovy
+++ grails-app/domain/neo4jSampleApp/core/Player.groovy
@@ -1,14 +1,20 @@
 package neo4jSampleApp.core
 
+import grails.compiler.GrailsCompileStatic
+
+@GrailsCompileStatic
 class Player extends BaseGraphDomain {
 
     String name
     Integer number
 
+    List attributes
+
     static belongsTo = [club: Club]
 
     static constraints = {
         name nullable: false, blank: false, unique: true
-        number nullable: false, validator: { it > 0 }
+        number nullable: false, validator: { (Integer) it > 0 }
+        attributes nullable: true
     }
 }
